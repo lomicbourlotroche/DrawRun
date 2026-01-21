69f0e00a98e4b414e0563543773cc44b234826af
swal-l <lomicbr@gmail.com>
2026-01-21 16:06:36 +0100

    Set app as testOnly in AndroidManifest.xml

--- app/src/main/java/com/drawrun/app/ui/screens/PlanningScreen.kt
+++ app/src/main/java/com/drawrun/app/ui/screens/PlanningScreen.kt
@@ -1,5 +1,6 @@
 package com.drawrun.app.ui.screens
 
+import androidx.compose.animation.*
 import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
