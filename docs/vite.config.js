4d6ad740be494768073be0f38d27c0280c0c2ee3
swal-l <lomicbr@gmail.com>
2026-01-22 20:32:42 +0100

    site error

--- docs/vite.config.js
+++ docs/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/DrawRun/'
+  base: './' // Relative paths work for local AND GitHub Pages
 })
